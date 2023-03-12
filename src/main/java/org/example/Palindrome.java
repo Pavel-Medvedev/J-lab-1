afdc5eea1b1d8c24fbd0329ba848d19fc704bbcb
Pavel-Medvedev <medvedev.pavel.al@outlook.com>
2023-03-12 11:12:09 -0700

    Выполнение лабораторной работы №1 по Java

--- src/main/java/org/example/Palindrome.java
+++ src/main/java/org/example/Palindrome.java
@@ -1,2 +1,19 @@
-package org.example;public class Palindrome {
+package org.example;
+
+public class Palindrome {
+    public static void main(String[] args){
+        for (int i = 0; i < args.length; i++)
+            if (isPalindrome(args[i]))
+                System.out.println(args[i] + " is palindrome.");
+            else System.out.println(args[i] + " isn\'t palindrome");
+    }
+    private static String reversString(String str){
+        String nStr = "";
+        for (int i = (int) str.length() - 1; i > -1; i--)
+            nStr += str.charAt(i);
+        return nStr;
+    }
+    private static boolean isPalindrome(String str){
+        return str.equals(reversString(str));
+    }
 }
--- src/main/java/org/example/Primes.java
+++ src/main/java/org/example/Primes.java
@@ -1,2 +1,19 @@
-package org.example;public class Primes {
+package org.example;
+
+public class Primes {
+    public static void main(String[] args) {
+        System.out.print("1 2");
+        for (byte i = 3; i < 101; i+=2)
+            if (isPrime(i))
+                System.out.print(" " + i);
+        System.out.println();
+    }
+    private static boolean isPrime(byte num) {
+        byte stop = (byte) (Math.sqrt(num) + 1);
+        for (byte i = 3; i < stop; i+=2)
+            if (num % i == 0)
+                return false;
+        return true;
+    }
+
 }
